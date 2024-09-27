612df0b2b55a914977cd3c3d28212268de29ba22
Daikus007 <moyafabian989@gmail.com>
2024-09-27 17:41:02 -0500

    Confiiguación inicial de react

--- app-logistica-v1/src/index.js
+++ app-logistica-v1/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './App.css';
-import Modal from 'react-modal';
-Modal.setAppElement('#root');
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-    <BrowserRouter>
+    <React.StrictMode>
         <App />
-    </BrowserRouter>
+    </React.StrictMode>
 );
 
+reportWebVitals();
